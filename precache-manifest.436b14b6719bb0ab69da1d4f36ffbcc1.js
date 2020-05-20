ad29db93f01e87b20dc749d2079d9355c92f0bf2
Sourish <sourish.njs@gmail.com>
2020-05-20 10:46:07 +0530

    Updates

---
+++ precache-manifest.436b14b6719bb0ab69da1d4f36ffbcc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5af58c298d77a3f4d35c9ae002031c40",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "c08f8ad1af12065e0d42",
+    "url": "/covid-tracker/static/css/main.48ec98fe.chunk.css"
+  },
+  {
+    "revision": "00c7f44054c3bb5d5723",
+    "url": "/covid-tracker/static/js/2.274dcba0.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker/static/js/2.274dcba0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c08f8ad1af12065e0d42",
+    "url": "/covid-tracker/static/js/main.7600bc69.chunk.js"
+  },
+  {
+    "revision": "4169042b9acd6e3b2ac2",
+    "url": "/covid-tracker/static/js/runtime-main.1f26073b.js"
+  },
+  {
+    "revision": "be3a9d7a9665dd9303efba587543b166",
+    "url": "/covid-tracker/static/media/image.be3a9d7a.PNG"
+  }
+]);
